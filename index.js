00ad4848a3531ffc0fc88b37bd686b0a5ee24373
alexbril2703 <alex@cuex.tech>
2026-03-11 16:15:03 +0100

    Complete practicum updates and homepage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,64 +9,63 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+const OBJECT_TYPE = '2-58885757';
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+  const url = `https://api.hubapi.com/crm/v3/objects/${OBJECT_TYPE}?properties=name&properties=type&properties=age`;
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
 
-// * Code for Route 2 goes here
+  try {
+    const resp = await axios.get(url, { headers });
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+    console.log(JSON.stringify(resp.data.results, null, 2));
 
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+    res.render('homepage', {
+      title: 'Pets Overview',
+      data: resp.data.results
+    });
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.send('Fout bij ophalen van custom object data.');
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', {
+    title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+  });
 });
-*/
 
+app.post('/update-cobj', async (req, res) => {
+  const url = `https://api.hubapi.com/crm/v3/objects/${OBJECT_TYPE}`;
+
+  const newObject = {
+    properties: {
+      name: req.body.name,
+      type: req.body.type,
+      age: req.body.age
+    }
+  };
+
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+
+  try {
+    await axios.post(url, newObject, { headers });
+    res.redirect('/');
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.send('Fout bij aanmaken van custom object record.');
+  }
+});
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
